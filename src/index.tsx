bdf6a9cde099f1057c70d9e5e3ec27066b674a28
shyrack <CodeBucketCore@gmail.com>
2025-07-17 22:15:42 +0200

    got react running

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,8 @@
-console.log("hello");
-const x = () => {};
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <div>hello world! 2</div>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(<App />);
